a346d5f703ca2caaf0d1432b97385ce6a748c962
duff907 <duff907@gmail.com>
2017-05-19 14:08:58 +0100

    started scoreboard

--- table-tennis/src/Scoreboard.js
+++ table-tennis/src/Scoreboard.js
@@ -5,14 +5,42 @@ class Scoreboard extends Component {
 
   constructor() {
     super();
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.registerScoreboard();
   }
 
   render() {
+    var playerScores = [];
+    if (this.state.scores) {
+      var scoreKeys = Object.keys(this.state.scores);
+      for (var i=0; i < scoreKeys.length; i++) {
+        console.log(scoreKeys[i]);
+        var playerScore = <p>{scoreKeys[i]} - {this.state.scores[scoreKeys[i]]}</p>
+        playerScores.push(playerScore)
+      }
+    }
+
     return (
       <div>
         <p>Scoreboard</p>
+        {playerScores}
       </div>
     );
+  }
+
+  registerScoreboard() {
+    var self = this;
+    this.socket = io('http://localhost:8081');
+    this.socket.emit('join', 'scoreboard');
+
+    this.socket.on('score:update', function(scores){
+      self.setState({scores: scores});
+      this.setState({scores: {tom: 5, calum: 4}});
+    });
   }
 }
 
