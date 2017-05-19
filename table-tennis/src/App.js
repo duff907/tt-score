2399aec59128d0fe8d9e3a81b5d5c69497e630d9
Tom Pennington <tompennington.92@gmail.com>
2017-05-19 11:24:23 +0100

    added react app

---
+++ table-tennis/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+let io = require('socket.io-client');
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      playerName: ""
+    }
+
+    this.handleNameSubmit = this.handleNameSubmit.bind(this);
+    this.updatePlayerName = this.updatePlayerName.bind(this);
+    this.connectUser = this.connectUser.bind(this);
+
+    this.socket;
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <input onChange={this.updatePlayerName} type="text" id="n" />
+        <a href="#" onClick={this.handleNameSubmit}>Play</a>
+      </div>
+    );
+  }
+
+  updatePlayerName(event) {
+    this.setState({
+      playerName: event.target.value
+    });
+  }
+
+  handleNameSubmit() {
+    this.connectUser(this.state.playerName);
+  }
+
+  connectUser(playerName) {
+    this.socket = io('http://localhost:8081');
+    console.log(this.socket);
+    this.socket.emit('join', playerName);
+  }
+}
+
+export default App;
